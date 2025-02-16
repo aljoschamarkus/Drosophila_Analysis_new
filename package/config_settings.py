cccfeab1b173976752d26fee55ba3589234a840c
Aljoscha Markus <aljoscha.markus@gmx.de>
2025-02-16 21:19:35 +0100

    Sorted structure and parameters.

--- package/config_settings.py
+++ package/config_settings.py
@@ -1,24 +1,43 @@
 # config_settings.py
 
-# Fixed parameters
-main_dir = "/Users/aljoscha/Downloads/2012_nompC_Crimson_WT_4min_50p_TRex_beta_2101" # Main directory with ./group and ./single subdirectories
-condition = ["group", "single"]
-genotype = ["nompCxCrimson", "WTxCrimson", "nompCxWT"]
+############################################################################################################
+# consistent between experiments
+############################################################################################################
+
 quality = [1296, 972] # png with quality[0] x quality[1] pixels
-dish_radius = 6.5
-group_size = 5
-data_len = 7191 # Number of frames
 FPS = 30
-stimulation= [50, 2, "µW/mm^2", 625,"nm"] # [percent max stimulation, max stimulation intensity, unit, wavelength, unit]
-stimulation_used = f"{stimulation[3]}{stimulation[4]}, {stimulation[0] * stimulation[1] * 0.01}{stimulation[2]}" # ...nm, ...µW/mm^2
+group_size = 5
+stimulation= [2, "µW/mm^2", 625,"nm"] # [percent max stimulation, max stimulation intensity, unit, wavelength, unit]
+group_type = ["RGN", "AIB", "AGB"]
+condition = ["group", "single"]
 
-# Chosen parameters
-circle_default = [7, 7, 6.5] # x midpoint, y midpoint, radius
+############################################################################################################
+# variable betweeen experiments
+############################################################################################################
+
+# Experiments
+dish_radius = 6.5 # cm
 bootstrap_reps = 2
-unit_xy = "cm"
-unit_speed = "cm/s"
+main_dir = "/Users/aljoscha/Downloads/2012_nompC_Crimson_WT_4min_50p_TRex_beta_2101" # Main directory with ./group and ./single subdirectories
+genotype = ["nompCxCrimson", "WTxCrimson", "nompCxWT"]
 
-# Chosen plot parameters
+# semi necessary
+circle_default = [7, 7, 6.5] # x midpoint, y midpoint, radius
+data_len = 7191 # Number of frames
+
+############################################################################################################
+# analysis
+############################################################################################################
+
+speed_threshold = [0, 3] # cm/s
+distance_threshold_encounter = 0.5 # cm
+encounter_distance_threshold = 0.5 # cm
+encounter_duration_threshold = [10, 1800] # frames
+
+############################################################################################################
+# plot
+############################################################################################################
+
 colors = [['red', 'blue', 'green'], ['salmon', 'cornflowerblue', 'mediumseagreen']]
 line_styles = {condition[0]: "-", condition[1]: "--"}  # Line styles for conditions
 markers = {condition[0]: "o", condition[1]: "s"}  # Markers for conditions
@@ -27,7 +46,16 @@ color_mapping = {
     for cond, row in zip(condition, colors)
     for geno, color in zip(genotype, row)
 }
-# Chosen Tresholds
-speed_threshold = [0, 3] # cm/s
-encounter_distance_treshold = 0.5 # cm
-encounter_duration_treshold = [0, 2000] # frames
+
+unit_xy = "cm"
+unit_speed = "cm/s"
+
+
+
+
+
+
+
+
+stimulation_used = f"{stimulation[2]}{stimulation[3]}, {stimulation_intensity[0] * stimulation[0] * 0.01}{stimulation[1]}" # ...nm, ...µW/mm^2
+
